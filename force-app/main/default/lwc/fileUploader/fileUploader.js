4da211557254093e285a17ffa82ef97a5e95ef77
Maurice J. Davis <mjdavis@attainpartners.com>
2025-08-13 12:56:41 -0400

    🔥 deploy/2025-08-13--1256PM – Add 'Is Currently Displayed' field and persist image

--- force-app/main/default/lwc/fileUploader/fileUploader.js
+++ force-app/main/default/lwc/fileUploader/fileUploader.js
@@ -1,33 +1,52 @@
 import { LightningElement, api, track } from 'lwc';
-import getLatestImageVersionId from '@salesforce/apex/FileUploaderController.getLatestImageVersionId';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getCurrentPhoto from '@salesforce/apex/FileUploaderController.getCurrentPhoto';
+import updatePhotoStatus from '@salesforce/apex/FileUploaderController.updatePhotoStatus';
 
 export default class FileUploader extends LightningElement {
     @api recordId;
     @track fileUrl;
     @track contentDocumentId;
-    allowMultiple = false;
 
     get acceptedFormats() {
         return ['.jpg', '.jpeg', '.png'];
     }
 
     connectedCallback() {
-        if (this.recordId) {
-            getLatestImageVersionId({ recordId: this.recordId })
-                .then(versionId => {
-                    if (versionId) {
-                        this.fileUrl = '/sfc/servlet.shepherd/version/renditionDownload?rendition=ORIGINAL_JPG&versionId=' + versionId;
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching image version:', error);
-                });
-        }
+        this.loadCurrentPhoto();
+    }
+
+    loadCurrentPhoto() {
+        getCurrentPhoto({ recordId: this.recordId })
+            .then(result => {
+                if (result) {
+                    this.contentDocumentId = result.ContentDocumentId;
+                    this.fileUrl = '/sfc/servlet.shepherd/version/renditionDownload?rendition=ORIGINAL_JPG&versionId=' + result.Id;
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 
     handleUploadFinished(event) {
-        const file = event.detail.files[0];
-        const versionId = file.contentVersionId;
-        this.fileUrl = '/sfc/servlet.shepherd/version/renditionDownload?rendition=ORIGINAL_JPG&versionId=' + versionId;
+        const uploadedFile = event.detail.files[0];
+        const documentId = uploadedFile.documentId;
+        updatePhotoStatus({ recordId: this.recordId, contentVersionId: uploadedFile.contentVersionId, documentId: documentId })
+            .then(() => {
+                this.fileUrl = '/sfc/servlet.shepherd/version/renditionDownload?rendition=ORIGINAL_JPG&versionId=' + uploadedFile.contentVersionId;
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Photo uploaded and set as current.',
+                    variant: 'success'
+                }));
+            })
+            .catch(error => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Upload failed',
+                    message: error.body.message,
+                    variant: 'error'
+                }));
+            });
     }
 }
